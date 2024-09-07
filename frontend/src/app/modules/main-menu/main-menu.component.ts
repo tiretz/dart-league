0f015cd3f198b6cf6270c9a0d7943f5e45c1fb37
Tim Retzlaff <retzlaff-tim@gmx.de>
2024-09-07 09:45:28 +0200

    Show new game button only if user is logged in.

--- frontend/src/app/modules/main-menu/main-menu.component.ts
+++ frontend/src/app/modules/main-menu/main-menu.component.ts
@@ -6,6 +6,11 @@ import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 
+import { Subscription } from 'rxjs';
+
+import { ITokenUser } from '../../core/models/token-user.interface';
+import { AuthService } from '../../core/services/auth.service';
+
 import { RunningGamesComponent } from './components/running-games/running-games.component';
 
 @Component({
@@ -15,4 +20,22 @@ import { RunningGamesComponent } from './components/running-games/running-games.
   templateUrl: './main-menu.component.html',
   styleUrl: './main-menu.component.scss',
 })
-export class MainMenuComponent {}
+export class MainMenuComponent {
+  protected isUserLoggedIn: boolean = false;
+
+  private userSubscription?: Subscription;
+
+  constructor(private readonly authService: AuthService) {}
+
+  ngOnDestroy(): void {
+    this.userSubscription?.unsubscribe();
+  }
+
+  ngOnInit(): void {
+    this.userSubscription = this.authService.user$.subscribe({
+      next: (user: ITokenUser | undefined) => {
+        this.isUserLoggedIn = !!user;
+      },
+    });
+  }
+}
