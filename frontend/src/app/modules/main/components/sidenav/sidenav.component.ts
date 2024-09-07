9680d3fb53d01258eede11db851ae01355401b86
Tim Retzlaff <retzlaff-tim@gmx.de>
2024-09-07 11:38:14 +0200

    Show settings tab only if user is logged in.

--- frontend/src/app/modules/main/components/sidenav/sidenav.component.ts
+++ frontend/src/app/modules/main/components/sidenav/sidenav.component.ts
@@ -6,6 +6,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenav } from '@angular/material/sidenav';
 
+import { Subscription } from 'rxjs';
+
+import { ITokenUser } from '../../../../core/models/token-user.interface';
+import { AuthService } from '../../../../core/services/auth.service';
+
 import { SidenavLinkComponent } from './components/sidenav-link/sidenav-link.component';
 
 @Component({
@@ -16,6 +21,24 @@ import { SidenavLinkComponent } from './components/sidenav-link/sidenav-link.com
   styleUrl: './sidenav.component.scss',
 })
 export class SidenavComponent {
+  protected isUserLoggedIn: boolean = false;
+
   @Input({ required: true })
   sidenav!: MatSidenav;
+
+  private userSubscription?: Subscription;
+
+  constructor(private readonly authService: AuthService) {}
+
+  ngOnDestroy(): void {
+    this.userSubscription?.unsubscribe();
+  }
+
+  ngOnInit(): void {
+    this.userSubscription = this.authService.user$.subscribe({
+      next: (user: ITokenUser | undefined) => {
+        this.isUserLoggedIn = !!user;
+      },
+    });
+  }
 }
--- frontend/src/app/shared/directives/mat-table-sorting-cache.directive.spec.ts
+++ frontend/src/app/shared/directives/mat-table-sorting-cache.directive.spec.ts
@@ -1,8 +1,13 @@
+import { MatSort } from '@angular/material/sort';
+
 import { MatTableSortingCacheDirective } from './mat-table-sorting-cache.directive';
+import { inject } from '@angular/core';
 
 describe('MatTableSortingCacheDirective', () => {
   it('should create an instance', () => {
-    const directive = new MatTableSortingCacheDirective();
+    const ms: MatSort = inject(MatSort);
+
+    const directive = new MatTableSortingCacheDirective(ms);
     expect(directive).toBeTruthy();
   });
 });
